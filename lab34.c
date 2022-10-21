938e38459ebfba88f5cf1600dae736d1043b234a
DeMiVL <85761093+DeMiVL@users.noreply.github.com>
2022-10-21 17:19:51 +0500

    add lab1

--- lab34.c
+++ lab34.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-float	Func(float);
+double	Func(double);
 
-float	Integration_Rect(float(*)(float), int, float, float);
-
-char RungeTest(float, float, float);
+double	Integration_Rect(double(*)(double), int, double, double);
 
 int main()
 {
-	float precision = .0000001;
-	float a = 0, b = 0.5;
-	float integral, Runge;
+	double precision = 1e-9;
+	double a = 0, b = 0.5;
+	double integral, integral2;
 	int n = 2;
 	do
 	{
 		integral = Integration_Rect(Func, n, a, b);
-		Runge = Integration_Rect(Func, 2 * n, a, b);
-		printf("%f %f %f\n", integral, Runge, fabs(Runge - integral) / 3);
+		integral2 = Integration_Rect(Func, 2 * n, a, b);
+		printf("%f %f %f\n", integral, integral2, fabs(integral2 - integral) / 3);
 		n += 2;
 	}
-	while(! RungeTest(integral, Runge, precision));
-	printf("Counted integral = %f\n", Runge);
+	while(fabs(integral2 - integral) / 3 >= precision);
+	printf("Counted integral = %f\n", integral2);
 	return 0;
 }
 
-float	Func(float x)
+double	Func(double x)
 {
     if (x <= 0.250001)
 	    return exp(sin(x));
@@ -33,20 +31,11 @@ float	Func(float x)
 	    return exp(x) - 1 / sqrt(x);
 }
 
-char RungeTest(float a, float b, float precision)
+double	Integration_Rect(double(*f)(double), int amount, double left, double right)
 {
-	if(fabs(a - b) / 3. < precision)
-	{
-		return 1;
-	}
-	return 0;
-}
-
-float	Integration_Rect(float(*f)(float), int amount, float left, float right)
-{
-	float step = (right - left) / amount;
-	float sum = 0.0;
-	float x = left;
+	double step = (right - left) / amount;
+	double sum = 0.0;
+	double x = left;
 	for(int i = 1; i <= amount; i++)
 	{
 		sum += step * f((x + x + step) / 2);
