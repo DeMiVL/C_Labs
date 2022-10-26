bbf0f066e49d41358ce8f3a007df34ddc666f214
user <you@example.com>
2022-10-26 12:48:20 +0500

    add lab1

--- lab34.c
+++ lab34.c
@@ -5,27 +5,27 @@ double	Func(double, double);
 
 int main()
 {
-	double precision = 1e-6;
-	double a = 0, b = 0.5;
+	double precision  = 1e-6;
+	double a = 0.0, b = 0.5;
 	double integral, integral2;
 	int n = 2;
 	do
 	{
-		integral = 0.0;
-		integral2 = 0.0;
+		integral    = 0.0;
+		integral2   = 0.0;
 		double step = (b - a) / n;
-		double x = a;
+		double x    = a;
 		for(int i = 1; i <= n; i++)
 		{
-			integral += step * Func((x + x + step) / 2, precision);
+			integral  += step * Func((x + x + step) / 2, precision);
 			integral2 += (step / 2) * Func((x + x + step / 2) / 2, precision);
 			integral2 += (step / 2) * Func((x + x + step / 2 + step) / 2, precision);
 			x += step;
 		}
-		printf("%f %f %f\n", integral, integral2, fabs(integral2 - integral) / 3);
+		printf("%f %f %f\n", integral, integral2, fabs(integral2 - integral) / 3.0);
 		n += 2;
 	}
-	while(fabs(integral - integral2) / 3. >= precision);
+	while(fabs(integral - integral2) / 3.0 >= precision);
 	printf("Counted integral = %f\n", integral2);
 	return 0;
 }
