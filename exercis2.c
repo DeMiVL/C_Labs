58f6babaf650fbfb16e58ef6635756a1bc92dd5c
DeMiVL <85761093+DeMiVL@users.noreply.github.com>
2022-12-07 14:14:31 +0500

    Update exercis2.c

--- exercis2.c
+++ exercis2.c
@@ -6,25 +6,21 @@ bool is_isogram(const char phrase[])
     int   len    = strlen(phrase);
     char  pow1[len + 1];
     pow1[0] = 0;
-    int i;
-    for (i = 0; i < len; i++)
-    {
-        pow1[i] = phrase[i];
-    }
-    pow1[len] = 0;
+    strcpy(pow1, phrase);
+    int i, j;
     for (i = 0; i < len - 1; i++)
-    {
-        if (pow1[i] > pow1[i + 1])
-        {
-            pow1[i]     += pow1[i + 1];
-            pow1[i + 1]  = pow1[i] - pow1[i + 1];
-            pow1[i]     -= pow1[i + 1];
-        }
-    }
+        for (j = 0; j < len - i - 1; j++)
+            if (pow1[j] > pow1[j + 1])
+                {
+                    pow1[j]     += pow1[j + 1];
+                    pow1[j + 1]  = pow1[j] - pow1[j + 1];
+                    pow1[j]     -= pow1[j + 1];
+                }
     for (i = 0; i < len - 1; i++)
     {
         if(pow1[i] == 32) continue;
         if(pow1[i] == pow1[i + 1]) return 0;
     }
+    
     return 1;
 }
